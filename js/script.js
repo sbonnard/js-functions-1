f2dbd8547da14515aebdbe574a141eac14e92d0a
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-05 11:24:31 +0200

    ex 1 and 2 done

--- js/script.js
+++ js/script.js
@@ -5,16 +5,36 @@
 
 console.log("1/ Implémentez une fonction qui retourne la somme de ses 2 paramètres.");
 
-console.log();
-console.log();
+/**
+ * Gives the sum of two numbers with an addition
+ * @param {number} number Declares a's value
+ * @returns {number} Result of addition of a + b
+ */
+function getSum(a) {
+    const b = 5
+    return a + b
+}
+
+console.log(getSum(5));
+console.log(getSum(10));
 
 
 /* ------------------------------------------------------*/
 
 console.log("2/ Implémentez une fonction qui retourne la plus petite des 2 valeurs passées en paramètre.");
 
-console.log();
-console.log();
+/**
+ * Gives smallest value comparing two values
+ * @param {number} a Declares a's value
+ * @returns {number} If a < b, returns = a. Else, returns = b
+ */
+function getMinNumber(a) {
+    const b = 12
+    return Math.min(a, b) 
+}
+
+console.log(getMinNumber(2));
+console.log(getMinNumber(72));
 
 
 /* ------------------------------------------------------*/
