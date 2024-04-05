8de085c4d87608b4a43275eed5109ba4137845f1
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-05 12:27:39 +0200

    exercice 3 done

--- js/script.js
+++ js/script.js
@@ -7,16 +7,16 @@ console.log("1/ Implémentez une fonction qui retourne la somme de ses 2 paramè
 
 /**
  * Gives the sum of two numbers with an addition
- * @param {number} number Declares a's value
+ * @param {*} a 
+ * @param {*} b 
  * @returns {number} Result of addition of a + b
  */
-function getSum(a) {
-    const b = 5
+function getSum(a, b) {
     return a + b
 }
 
-console.log(getSum(5));
-console.log(getSum(10));
+console.log(getSum(5, 7));
+console.log(getSum(10, 12));
 
 
 /* ------------------------------------------------------*/
@@ -25,32 +25,73 @@ console.log("2/ Implémentez une fonction qui retourne la plus petite des 2 vale
 
 /**
  * Gives smallest value comparing two values
- * @param {number} a Declares a's value
+ * @param {number} a
+ * @param {number} b
  * @returns {number} If a < b, returns = a. Else, returns = b
  */
-function getMinNumber(a) {
-    const b = 12
-    return Math.min(a, b) 
+function getMinNumber(a, b) {
+    return Math.min(a, b)
 }
 
-console.log(getMinNumber(2));
-console.log(getMinNumber(72));
+console.log(getMinNumber(2, 85));
+console.log(getMinNumber(72, 5));
 
 
 /* ------------------------------------------------------*/
 
 console.log("3/ Implémentez une fonction qui retourne la dernière valeur d'un tableau.");
 
-console.log();
-console.log();
+const tableName = [
+    'Henry',
+    'Patrick',
+    'Minerva',
+    'Jean-Luc',
+    'Pedro',
+    'Simone',
+    'Gudule',
+    'Victor',
+    'Billie'
+]
+
+const tableNameScd = [
+    'Jacqueline',
+    'Julius',
+    'Marcel',
+    'Céline',
+    'Raymonde',
+    'Casper'
+]
+
+/**
+ * Gives last value entry of an array determined by a's value.
+ * @param {array} array Enter the array of your choice.
+ * @returns {name} last value of the array.
+ */
+function getLastTableValue(array) {
+    const i = array.length;
+    return array[i-1]
+}
+
+console.log(getLastTableValue(tableName));
+console.log(getLastTableValue(tableNameScd));
 
 
 /* ------------------------------------------------------*/
 
 console.log("4/ Implémentez une fonction qui retourne la valeur en entrée augmentée d'un pourcentage d'augmentation.");
 
-console.log();
-console.log();
+/**
+ * Get the increase of a by using a percentage of 25%.
+ * @param {number} a 
+ * @returns 
+ */
+function getIncreasePercent(a) {
+    const tax = 1.25;
+    return a * tax
+}
+
+console.log(getIncreasePercent(47));
+console.log(getIncreasePercent(82));
 
 
 /* ------------------------------------------------------*/
